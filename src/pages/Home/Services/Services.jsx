0f42138caacf484c12b48b4776f9c556b989886b
samudraon <samudraon@gmailcom>
2023-05-11 00:33:26 +0600

    login

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -16,7 +16,7 @@ const Services = () => {
             <div className="text-center">
                 <h3 className="text-3xl font-bold text-orange-600">Our Services</h3>
                 <h2 className="text-5xl ">Our Services Area</h2>
-                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
